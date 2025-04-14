e98b0442a4c8b77bad8ad12866c93d4ed8328e80
Darkonno <galaktikatst@gmail.com>
2025-04-14 10:37:00 +0500

    9

--- нач.py
+++ нач.py
@@ -12,4 +12,4 @@ for naum_fofto in neim:
     position = (hard, shir)
     fofto.paste(mark, position, mask=mark)
     fofto.save(os.path.join('гот', f"{i}.jpg" ))
-print("готово")
+print("Complite   9")
